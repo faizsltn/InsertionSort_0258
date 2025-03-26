48974fe87712873660223c32c0dc0734cf46d922
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-03-27 00:57:21 +0700

    prosedur dan tampilan output

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -39,3 +39,17 @@ void InsertionSort(){
         arr[j+1] = temp;
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1){
+            cout << "-->";
+        }
+    }
+}
