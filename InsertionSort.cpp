69f5203d6e372d8ed4901b90e7f652e89b3cac53
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-03-27 00:53:04 +0700

    membuat variabel i,temp,j serta perulangan insertionsort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -26,3 +26,16 @@ void input(){
         cin >> arr[i];
     }
 }
+
+void InsertionSort(){
+    int i, temp, j;
+    for(i = 1; i <= n-1; i++){
+        temp = arr[i];
+        j = i -1;
+        while(j > 0 && arr[j] > temp){
+            arr[j + 1] = arr [j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
